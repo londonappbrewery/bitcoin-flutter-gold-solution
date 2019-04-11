e78559a91e4697d0d9068c71e0d19c7f6ccfaa16
Angela <8798027+angelabauer@users.noreply.github.com>
2019-04-11 18:27:21 +0100

    Initial

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -25,30 +25,28 @@ const List<String> currenciesList = [
   'ZAR'
 ];
 
-const List<String> cryptoList = ['BTC', 'ETH', 'LTC'];
+const List<String> cryptoList = [
+  'BTC',
+  'ETH',
+  'LTC',
+];
 
 const bitcoinAverageURL =
-    'https://apiv2.bitcoinaverage.com/indices/gsdslobal/ticker';
+    'https://apiv2.bitcoinaverage.com/indices/global/ticker';
 
 class CoinData {
   Future getCoinData(String selectedCurrency) async {
-    //4: Use a for loop here to loop through the cryptoList and request the data for each of them in turn.
-    //5: Return a Map of the results instead of a single value.
-    Map<String, String> cryptoPrices = {};
-    for (String crypto in cryptoList) {
-      //Update the URL to use the crypto symbol from the cryptoList
-      String requestURL = '$bitcoinAverageURL/$crypto$selectedCurrency';
-      http.Response response = await http.get(requestURL);
-      if (response.statusCode == 200) {
-        var decodedData = jsonDecode(response.body);
-        double lastPrice = decodedData['last'];
-        //Create a new key value pair, with the key being the crypto symbol and the value being the lastPrice of that crypto currency.
-        cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
-      } else {
-        print(response.statusCode);
-        throw 'Problem with the get request';
-      }
+    //TODO 4: Use a for loop here to loop through the cryptoList and request the data for each of them in turn.
+    //TODO 5: Return a Map of the results instead of a single value.
+    String requestURL = '$bitcoinAverageURL/BTC$selectedCurrency';
+    http.Response response = await http.get(requestURL);
+    if (response.statusCode == 200) {
+      var decodedData = jsonDecode(response.body);
+      var lastPrice = decodedData['last'];
+      return lastPrice;
+    } else {
+      print(response.statusCode);
+      throw 'Problem with the get request';
     }
-    return cryptoPrices;
   }
 }
