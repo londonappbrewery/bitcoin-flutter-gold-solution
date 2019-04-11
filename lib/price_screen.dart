3220bd26133ae2efe6cdfdbe8f2faed673aaa32a
Angela Yu <8798027+angelabauer@users.noreply.github.com>
2019-04-11 18:31:16 +0100

    Update price_screen.dart

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -169,7 +169,7 @@ class CryptoCard extends StatelessWidget {
         child: Padding(
           padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
           child: Text(
-            '1 BTC = $value $selectedCurrency',
+            '1 $cryptoCurrency = $value $selectedCurrency',
             textAlign: TextAlign.center,
             style: TextStyle(
               fontSize: 20.0,
