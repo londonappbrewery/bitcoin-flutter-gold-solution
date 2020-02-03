ae5deb9bc8a776890ce1473737ceed2d9bcbc36f
TheMuellenator <TheMuellenator@users.noreply.github.com>
2020-02-03 16:11:35 +0000

    Updating to coinapi.io

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -27,8 +27,8 @@ const List<String> currenciesList = [
 
 const List<String> cryptoList = ['BTC', 'ETH', 'LTC'];
 
-const bitcoinAverageURL =
-    'https://apiv2.bitcoinaverage.com/indices/global/ticker';
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = 'YOUR-API-KEY-HERE';
 
 class CoinData {
   Future getCoinData(String selectedCurrency) async {
@@ -37,11 +37,12 @@ class CoinData {
     Map<String, String> cryptoPrices = {};
     for (String crypto in cryptoList) {
       //Update the URL to use the crypto symbol from the cryptoList
-      String requestURL = '$bitcoinAverageURL/$crypto$selectedCurrency';
+      String requestURL =
+          '$coinAPIURL/$crypto/$selectedCurrency?apikey=$apiKey';
       http.Response response = await http.get(requestURL);
       if (response.statusCode == 200) {
         var decodedData = jsonDecode(response.body);
-        double lastPrice = decodedData['last'];
+        double lastPrice = decodedData['rate'];
         //Create a new key value pair, with the key being the crypto symbol and the value being the lastPrice of that crypto currency.
         cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
       } else {
